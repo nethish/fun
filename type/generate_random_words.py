106aad32e9f0bd88bc3cec40a90938c23d524c02
nethish <nethish259@gmail.com>
2022-12-03 09:59:51 +0530

    Initial commit

---
+++ type/generate_random_words.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+# Program to generate random words + numbers combination
+# Paste the words in keybr
+
+import random
+
+def filter_words(words, max_length=5):
+  return list(filter(lambda x: len(x) <= max_length, words))
+
+
+words = open('./words.txt', 'r').read().split('\n')
+words = filter_words(words)
+N = len(words)
+
+REQ_WORDS = 1000
+
+for i in range(REQ_WORDS):
+  print(words[random.randint(0, N)], random.randint(10, 100), end = ' ')
