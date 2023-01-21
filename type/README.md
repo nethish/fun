ce219d0a134c15524647259c765d1f0e4cb82b74
nethish <nethish259@gmail.com>
2023-01-22 01:03:13 +0530

    Added ously words

---
+++ type/README.md
@@ -0,0 +1,8 @@
+## Steps
+1. `./generate_random_words.py ously.txt`
+2. `xclip -sel clip -i custom_words.txt`
+3. Paste it in keybr.com
+
+
+## TODO
+1. Take multiple file inputs
--- type/generate_random_words.py
+++ type/generate_random_words.py
@@ -1,23 +1,31 @@
 #!/bin/python3
 
-# Program to generate random words + numbers combination
-# Paste the words in keybr
-
 import random
 import sys
+
+FILE = sys.argv[1]
+
+REQ_WORDS = 1000
 
 def filter_words(words, max_length=5):
   return list(filter(lambda x: len(x) <= max_length, words))
 
 
-words = open('./words.txt', 'r').read().split('\n')
-words = filter_words(words)
+words = open(FILE, 'r').read().split('\n')
+# words = filter_words(words)
 N = len(words)
 
-REQ_WORDS = 1000
+print("Number of words:", N)
+
+# nums = [4, 5, 6, 7, 45, 56, 67, 46, 47, 57]
+# nums = [str(i) for i in nums]
+# nums += [i[::-1] for i in nums]
+
+
+def print_rand(word_list):
+  print(word_list[random.randint(0, len(word_list) - 1)], end = ' ')
+
 
 sys.stdout = open('./custom_words.txt', 'w+')
-
-
 for i in range(REQ_WORDS):
-  print(words[random.randint(0, N)], random.randint(10, 100), end = ' ')
+  print_rand(words)
